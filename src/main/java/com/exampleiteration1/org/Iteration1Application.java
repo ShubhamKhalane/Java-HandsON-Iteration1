6833271e2cf168ae8ce4480c768f7f45d7c59479
ShubhamKhalane <snkhalanesk@gmail.com>
2023-06-05 18:10:04 +0530

    Initial commit

---
+++ src/main/java/com/exampleiteration1/org/Iteration1Application.java
@@ -0,0 +1,13 @@
+package com.exampleiteration1.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iteration1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iteration1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampleiteration1/org/Iteration1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleiteration1.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iteration1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
